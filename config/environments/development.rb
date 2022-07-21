f5e6d2d871628f32929cb1d9544e1b3c9138bac5
shinju0514 <shinju0514yellow@gmail.com>
2022-07-21 13:41:24 +0000

    tag

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "f35e2a4ccf0b4b8d81df5d7369a09eec.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
